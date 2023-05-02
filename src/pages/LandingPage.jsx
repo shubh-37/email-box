e735a6fe9bfce133f137782acfbb63930678a2b4
shubh-37 <shubh.arya37@gmail.com>
2023-05-03 01:29:26 +0530

    testing filters and content

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Filters from "../components/Filters";
+
+export default function LandingPage() {
+  return (
+    <React.Fragment>
+      <Filters />
+      <h2>this is inbox</h2>
+    </React.Fragment>
+  );
+}
