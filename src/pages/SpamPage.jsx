b1eb8a25628d3f11a8bfec99b4571e8fecfd5970
shubh-37 <shubh.arya37@gmail.com>
2023-05-03 01:31:54 +0530

    testing pages

---
+++ src/pages/SpamPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SpamPage(){
+    return (
+        <React.Fragment>
+            <h1>This is the spam page</h1>
+        </React.Fragment>
+    )
+}
