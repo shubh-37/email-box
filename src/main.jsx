e8f5a489b77e884e97f1d2800d7bd225b7db77d0
shubh-37 <shubh.arya37@gmail.com>
2023-05-03 20:50:10 +0530

    wrapped the mail provider around App

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
+import MailProvider from "./contexts/MailProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <MailProvider>
+        <App />
+      </MailProvider>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
