b030b1cec6f50c161b0327e64d955b44cda5a844
shubh-37 <shubh.arya37@gmail.com>
2023-05-03 20:32:43 +0530

    passed mail data as props to inbox

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,77 @@ import SpamPage from "./pages/SpamPage";
 import TrashPage from "./pages/TrashPage";
 
 function App() {
+  const mails = [
+    {
+      mId: "guid-1",
+      unread: false,
+      isStarred: false,
+      subject: "Training Program",
+      content:
+        "About Microsoft Virtual Academy<br/>Microsoft Virtual Academy provides free online training by world-class experts to help you build your technical skills and advance your career. Make it your destination of choice to get started on the latest Microsoft technologies and join this vibrant community.",
+    },
+    {
+      mId: "guid-2",
+      unread: true,
+      isStarred: false,
+      subject: "Empower your future",
+      content:
+        "We foster our pipeline of future leaders with 47 employee networks and 7 global employee resource groups, servicing an active community of thousands across Microsoft",
+    },
+    {
+      mId: "guid-3",
+      unread: true,
+      isStarred: true,
+      subject: "Pre Approved Loan",
+      content: "Congratulations ! <u>Credit card<u> is being shipped to you today!",
+    },
+    {
+      mId: "guid-4",
+      unread: true,
+      isStarred: false,
+      subject: "You won a lottery!",
+      content:
+        "You have just won the New York official lottery. Please send us your address so that we may start the transfer.",
+    },
+    {
+      mId: "guid-5",
+      unread: true,
+      isStarred: false,
+      subject: "Invact Finance Program",
+      content:
+        "About Microsoft Virtual Academy<br/>Microsoft Virtual Academy provides free online training by world-class experts to help you build your technical skills and advance your career. Make it your destination of choice to get started on the latest Microsoft technologies and join this vibrant community.",
+    },
+    {
+      mId: "guid-6",
+      unread: true,
+      isStarred: false,
+      subject: "neoG Camp Interview",
+      content:
+        "We foster our pipeline of future leaders with 47 employee networks and 7 global employee resource groups, servicing an active community of thousands across Microsoft",
+    },
+    {
+      mId: "guid-7",
+      unread: true,
+      isStarred: false,
+      subject: "Pre Approved Loan 1",
+      content: "Congratulations ! <u>Credit card<u> is being shipped to you today!",
+    },
+    {
+      mId: "guid-8",
+      unread: true,
+      isStarred: false,
+      subject: "You won a lottery! 8th time",
+      content:
+        "You have just won the New York official lottery. Please send us your address so that we may start the transfer.",
+    },
+  ];
   return (
     <React.Fragment>
       <h1 className="heading">Shubh Arya's mail box</h1>
       <div className="parent">
         <Header />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage mails={mails} />} />
           <Route path="/spam" element={<SpamPage />} />
           <Route path="/trash" element={<TrashPage />} />
         </Routes>
