ea631ec41f57f8052344130d22fb1c094ab404cd
shubh-37 <shubh.arya37@gmail.com>
2023-05-04 20:31:58 +0530

    displayed trash mails

--- src/pages/TrashPage.jsx
+++ src/pages/TrashPage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import DisplayMail from "../components/DisplayMail";
+import { mailContext } from "../contexts/MailProvider";
 
-export default function TrashPage(){
-    return (
-        <React.Fragment>
-            <h1>This is the trash page</h1>
-        </React.Fragment>
-    )
+export default function TrashPage() {
+  const { state } = useContext(mailContext);
+  return (
+    <React.Fragment>
+      <h1>This is the trash page</h1>
+      <p>{state.bin.length=== 0 ? "There are no deleted emails" : <></>}</p>
+      <ul>
+        {state.bin.map((mail) => (
+          <DisplayMail mail={mail} detail={false} />
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 }
