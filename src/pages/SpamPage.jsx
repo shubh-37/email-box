2685f47fbe521639050331d45f8b78eaa1941cb3
shubh-37 <shubh.arya37@gmail.com>
2023-05-04 20:31:32 +0530

    displayed spam mails

--- src/pages/SpamPage.jsx
+++ src/pages/SpamPage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import DisplayMail from "../components/DisplayMail";
+import { mailContext } from "../contexts/MailProvider";
 
-export default function SpamPage(){
-    return (
-        <React.Fragment>
-            <h1>This is the spam page</h1>
-        </React.Fragment>
-    )
+export default function SpamPage() {
+  const { state } = useContext(mailContext);
+  return (
+    <React.Fragment>
+      <h1>This is the spam page</h1>
+      <p>{state.spam.length === 0 ? "There are no spam emails" : <></>}</p>
+      <ul>
+        {state.spam.map((mail) => (
+          <DisplayMail mail={mail} detail={false} />
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 }
