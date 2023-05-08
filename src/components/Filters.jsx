69419d057ab569bb491433d218b09e0b17ed3680
shubh-37 <shubh.arya37@gmail.com>
2023-05-09 02:44:27 +0530

    added event handlers on checkboxes

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import { mailContext } from "../contexts/MailProvider";
 
 export default function Filters() {
+  const {dispatch, state: { isStarredFilter, unreadFilter }} = useContext(mailContext);
   return (
     <React.Fragment>
       <fieldset className="filter-box">
         <legend>Filters</legend>
         <label>
-          <input type="checkbox" name="unread" id="" />
+          <input type="checkbox" name="unread" value="unread" checked ={unreadFilter} onChange={(e) => dispatch({type: "FILTER", payload: e.target.value})}/>
           Show unread emails
         </label>
         <label htmlFor="">
-          <input type="checkbox" name="starred" id="" />
+          <input type="checkbox" name="starred" value="isStarred" checked= {isStarredFilter}onChange={(e) => dispatch({type: "FILTER", payload: e.target.value})}/>
           Show starred emails
         </label>
       </fieldset>
