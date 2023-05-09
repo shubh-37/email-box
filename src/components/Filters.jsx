6039c02bba7f88fdb1a9067b52bdf25c144420b2
shubh-37 <shubh.arya37@gmail.com>
2023-05-10 00:30:33 +0530

    style: margin to labels

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -8,12 +8,12 @@ export default function Filters() {
     <React.Fragment>
       <div className="child-1">
       <fieldset className="filter-box">
-        <legend>Filters</legend>
-        <label>
+        <legend style={{fontSize: "1.2rem"}}>Filters</legend>
+        <label style = {{margin: '0.9rem'}}>
           <input type="checkbox" name="unread" value="unread" checked ={unreadFilter} onChange={(e) => dispatch({type: "FILTER", payload: e.target.value})}/>
           Show unread emails
         </label>
-        <label htmlFor="">
+        <label htmlFor="" style = {{margin: '0.9rem'}}>
           <input type="checkbox" name="starred" value="isStarred" checked= {isStarredFilter}onChange={(e) => dispatch({type: "FILTER", payload: e.target.value})}/>
           Show starred emails
         </label>
