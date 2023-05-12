cb0013c0c4bf12468178478dc8d0e689d139c02d
shubh-37 <shubh.arya37@gmail.com>
2023-05-12 13:35:56 +0530

    refact: added parent div for common styling

--- src/pages/SpamPage.jsx
+++ src/pages/SpamPage.jsx
@@ -7,12 +7,14 @@ export default function SpamPage() {
   const { state } = useContext(mailContext);
   return (
     <React.Fragment>
+      <div className="sub-parent">
       <h3>{state.spam.length === 0 ? "There are no spam emails" : <></>}</h3>
       <ul>
         {state.spam.map((mail) => (
           <DisplayMail mail={mail} detail={false} />
         ))}
       </ul>
+      </div>
     </React.Fragment>
   );
 }
--- src/pages/TrashPage.jsx
+++ src/pages/TrashPage.jsx
@@ -7,12 +7,14 @@ export default function TrashPage() {
   const { state } = useContext(mailContext);
   return (
     <React.Fragment>
+      <div className="sub-parent">
       <h3>{state.bin.length=== 0 ? "There are no deleted emails" : <></>}</h3>
       <ul>
         {state.bin.map((mail) => (
           <DisplayMail mail={mail} detail={false} />
         ))}
       </ul>
+      </div>
     </React.Fragment>
   );
 }
